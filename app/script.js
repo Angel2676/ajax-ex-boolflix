30a43dae965b1c709901eb7bddc8593a64ecd5d2
Angelo <angelo_1976@icloudcom>
2020-09-02 21:57:31 +0200

    keydown

--- app/script.js
+++ app/script.js
@@ -13,52 +13,60 @@ $(document).ready(function(){
 
 
 
-    // // 1 Step - Creazione searchbar che al click prende il valore
-    $("button#button").click(function(){
-        var input = $("#valoriInput").val();
-        $("#lista").empty();   
-        // console.log(input); // Passaggio ok
+    //  1 Step - Creazione searchbar che al click prende il valore
+            $("button#button").click(function(){
+                attiva()
+            }) // Fine funzione
+            $("#valoriInput").keydown(function(){
+                if (event.which==13 || event.keyCode==13) {
+                    attiva()
+                }
 
-
-
-    // 2 step - cercare nell'Api il titolo
-    $.ajax({
-        url: "https://api.themoviedb.org/3/search/movie",
-        method : "GET",
-        data: {
-            api_key : "86441f8205c2837900332bf796f193e9",
-            query: input,
-            language: "it-IT"
-
-                }, // fine data
-
-        success: function(risposta){
-            for (var i = 0; i < risposta.results.length; i++) {
-                var source = $("#entry-template").html();
-                var template = Handlebars.compile(source);
-                var context =
-                            {
-                                title: risposta.results[i].title,
-                                original_title : risposta.results[i].original_title,
-                                original_language : risposta.results[i].original_language,
-                                vote_average : risposta.results[i].vote_average,
-
-
-                            };
-                var html = template(context);
-                $("#lista").append(html);
-
-
-
-                } // fine del for
-        }, // Fine success function
-        error: function(){
-            alert("Si è verificato un errore");
-        }
-    }); // Fine ajax
-}); // fine presa valore
-
-// 3 Step - viasualizzazione a schermo
-
-
+            }) // fine keydown
 }); // Fine document
+
+
+// ************FUNZIONI**************
+
+        function attiva(){ // Chiudere funzione
+            var input = $("#valoriInput").val();
+            $("#lista").empty();
+            // console.log(input); // Passaggio ok
+
+            $.ajax(
+                {
+            url: "https://api.themoviedb.org/3/search/movie",
+            method : "GET",
+            data: {
+                api_key : "86441f8205c2837900332bf796f193e9",
+                query: input,
+                language: "it-IT"
+
+                    }, // fine data
+
+            success: function(risposta){
+        for (var i = 0; i < risposta.results.length; i++) {
+            var source = $("#entry-template").html();
+            var template = Handlebars.compile(source);
+            var context =
+                        {
+                            title: risposta.results[i].title,
+                            original_title : risposta.results[i].original_title,
+                            original_language : risposta.results[i].original_language,
+                            vote_average : risposta.results[i].vote_average,
+
+
+                        };
+            var html = template(context);
+            $("#lista").append(html);
+
+
+
+            } // fine del for
+        }, // Fine success function
+            error: function(){
+                alert("Si è verificato un errore");
+                }
+    }); // Fine ajax
+
+    }; // fine presa valore
